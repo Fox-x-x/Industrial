f7ef98090d50a4a4c6bd4adc1b1c89d9ccee63ec
Pavel Yurkov <gemini86@list.ru>
2021-02-14 22:15:14 +0200

    task has been done:)

---
+++ GCD practice/TableViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  GCD practice
+//
+//  Created by Pavel Yurkov on 14.02.2021.
+//
+
+import UIKit
+import SwiftyJSON
+import SwiftSpinner
+
+class TableViewController: UIViewController {
+    
+    private var photoURLs: [String] = []
+    
+    private lazy var myCoolTableView: UITableView = {
+        let tv = UITableView()
+        tv.dataSource = self
+        tv.delegate = self
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        return tv
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "GCD test table"
+        setupLayout()
+        getURLsFromServer()
+        
+    }
+    
+    func getURLsFromServer() {
+        if let url = URL(string: "https://jsonplaceholder.typicode.com/photos") {
+            let queue = DispatchQueue.global(qos: .utility)
+            queue.async {
+                let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+                    DispatchQueue.main.async {
+                        SwiftSpinner.show("Loading data...", animated: true)
+                    }
+                    guard let data = data else { return }
+                    self.parseJSON(data)
+                }
+
+                task.resume()
+            }
+        }
+    }
+    
+    func parseJSON(_ serverData: Data) {
+        
+        let json = JSON(serverData)
+        
+        for (_, element) in json.enumerated() {
+            if let url = element.1["url"].string {
+                photoURLs.append(url)
+            }
+        }
+        DispatchQueue.main.async {
+            SwiftSpinner.hide()
+            self.myCoolTableView.reloadData()
+        }
+    }
+
+}
+
+extension TableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+}
+
+extension TableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photoURLs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = myCoolTableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        cell.textLabel?.text = photoURLs[indexPath.row]
+        
+        return cell
+    }
+    
+}
+
+private extension TableViewController {
+    
+    func setupLayout() {
+        view.addSubviewWithAutolayout(myCoolTableView)
+        
+        let constraints = [
+            myCoolTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            myCoolTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            myCoolTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            myCoolTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'GCD practice' do
+  use_frameworks!
+
+  # Pods for GCD practice
+  pod 'SwiftyJSON', '~> 4.0'
+  pod "SwiftSpinner"
+
+end
