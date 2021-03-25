92475c1588086494d8354f1e9a6b8f89e228eb50
Pavel Yurkov <gemini86@list.ru>
2021-03-25 20:24:17 +0200

    Task 1 and 2 implemented

---
+++ Navigation/Networking/ToDoItem.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItem.swift
+//  Navigation
+//
+//  Created by Pavel Yurkov on 24.03.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    
+    var userId: Int
+    var id: Int
+    var title: String
+    var completed: Bool
+    
+}
--- Navigation/ViewControllers/InfoViewController.swift
+++ Navigation/ViewControllers/InfoViewController.swift
@@ -21,13 +21,80 @@ class InfoViewController: UIViewController {
         button.addTarget(self, action: #selector(deletePostButtonTapped), for: .touchUpInside)
         return button
     }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.sizeToFit()
+        label.text = "???"
+        return label
+    }()
+    
+    private lazy var planetLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.sizeToFit()
+        label.text = "???"
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBlue
         setupLayout()
+        summonTheDevil()
+        getPlanet()
 
+    }
+    
+    private func summonTheDevil() {
+        
+        if let url = URL(string: "https://jsonplaceholder.typicode.com/todos/4") {
+            NetworkManager.dataTask(url: url) { (data) in
+                if let result = data {
+                    do {
+                        let object = try NetworkManager.JSONToObject(json: result)
+                        if let dictionary = object {
+                            let toDoItem = ToDoItem(userId: dictionary["userId"] as? Int ?? -1,
+                                                    id: dictionary["id"] as? Int ?? -1,
+                                                    title: dictionary["title"] as? String ?? "error",
+                                                    completed: dictionary["completed"] as? Bool ?? false
+                            )
+                            DispatchQueue.main.async {
+                                self.titleLabel.text = toDoItem.title
+                            }
+                        }
+                    } catch {
+                        self.titleLabel.text = "error"
+                        handleApiError(error: .other, vc: self)
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    private func getPlanet() {
+        
+        if let url = URL(string: "https://swapi.dev/api/planets/1/") {
+            NetworkManager.dataTask(url: url) { (data) in
+                if let result = data {
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    if let planet = try? decoder.decode(Planet.self, from: result) {
+                        DispatchQueue.main.async {
+                            self.planetLabel.text = planet.name + " orbital period: " + planet.orbitalPeriod
+                        }
+                    }
+                }
+            }
+        }
+        
     }
     
     @objc private func deletePostButtonTapped() {
@@ -44,13 +111,24 @@ class InfoViewController: UIViewController {
     }
     
     private func setupLayout() {
-        view.addSubviewWithAutolayout(deleteButton)
+        view.addSubviews(deleteButton, titleLabel, planetLabel)
         
         let constraints = [
             deleteButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             deleteButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             deleteButton.widthAnchor.constraint(equalToConstant: 100),
-            deleteButton.heightAnchor.constraint(equalToConstant: 30)
+            deleteButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            titleLabel.topAnchor.constraint(equalTo: deleteButton.bottomAnchor, constant: 32),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            titleLabel.heightAnchor.constraint(equalToConstant: 18),
+            
+            planetLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            planetLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            planetLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            planetLabel.heightAnchor.constraint(equalToConstant: 18)
+            
         ]
         
         NSLayoutConstraint.activate(constraints)
